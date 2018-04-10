83e4b076d042cf39aaad1fd30f591d7131a6ce5a
Charlotte M. Marsh <cmarsh@uni-bremen.de>
2018-04-11 00:35:27 +0200

    Repaired algorithm

--- solvers.py
+++ solvers.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+
 """Routines for solving a linear system of equations."""
 import numpy as np
-
 
 def gaussian_eliminate(aa, bb):
     """Solves a linear system of equations (Ax = b) by Gauss-elimination
@@ -13,7 +14,56 @@ def gaussian_eliminate(aa, bb):
         Vector xx with the solution of the linear equation or None
         if the equations are linearly dependent.
     """
-    nn = aa.shape[0]
-    xx = np.zeros((nn,), dtype=float)
+
+    nn = aa.shape[0] #nn is the number of equations given
+
+    bb.shape = (nn, 1) #bb transpose
+
+    cc = np.hstack([aa, bb]) #merge the matrices
+
+    print(cc)
+
+
+
+    ii = 0 #iterating index
+    for ii in range(0, nn):         #great for-loop
+
+        if cc[ii, ii] == 0: #if diagonalelement is zero switch the row with the next row that isn't zero
+            jj = ii+1
+            while cc[jj, ii] is None:
+                if jj <= nn-1:
+                    jj = jj+1
+                else:
+                    print('The LEQ is not solvable') #if there is none, the LEQ isn't solvable
+            else:
+                 cc[[ii, jj], :] = cc[[jj, ii], :]
+
+        else:
+            print(cc)
+
+        print(cc)
+
+        cc[ii, :] = cc[ii, :] / cc[ii, ii] #make diagonalelement 1
+
+        print(cc)
+
+
+        #eliminate rest of the i-column
+        for jj in range(0, nn):
+            if jj == ii:
+                continue
+            else:
+                cc[jj, :] = cc[jj, :] - cc[ii, :] * cc[jj, ii]
+
+    print(cc)
+
+    xx = cc[:, nn]
+    print('The solution of the LES is:')
+    print(xx)
+
     return xx
 
+aa = np.array([[2.0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
+bb = np.array([1.0, 4.0, 2.0])
+
+xx = gaussian_eliminate(aa, bb)
