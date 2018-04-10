73d6466ae8649d2a9c59970a6ff0a1a90a02b15e
Charlotte M. Marsh <cmarsh@uni-bremen.de>
2018-04-11 01:08:47 +0200

    No change made

--- firststeps.py
+++ firststeps.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-aa = np.array([[0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
-bb = np.array([1.0, 4.0, 2.0]) 
+aa = np.array([[2.0, 4.0, 4.0], [5.0, 4.0, 2.0], [1.0, 2.0, -1.0]])
+bb = np.array([1.0, 4.0, 2.0])
 
 nn = aa.shape[0] #nn is the number of equations given
 
@@ -17,17 +17,15 @@ ii=0 #iterating index
 for ii in range (0,nn):         #great for-loop
 
     if cc[ii,ii]==0: #if diagonalelement is zero switch the row with the next row that isn't zero
-            jj = ii+1 
+            jj = ii+1
             while cc[jj,ii] is None:
                 if jj<=nn-1:
                     jj=jj+1
                 else:
-                    print('The LEQ is not solvable') #if there is none, the LEQ isn't solvable
+                    print('The LES is not solvable') #if there is none, the LEQ isn't solvable
             else:
                  cc[[ii,jj],:]=cc[[jj,ii],:]
 
-    else:
-        print(cc)
 
     print(cc)
 
@@ -40,7 +38,7 @@ for ii in range (0,nn):         #great for-loop
         if jj==ii:
             continue
         else:
-            cc[jj,:]=cc[jj,:]-cc[ii,:]*cc[jj,ii]   
+            cc[jj,:]=cc[jj,:]-cc[ii,:]*cc[jj,ii]
 
 print(cc)
 
